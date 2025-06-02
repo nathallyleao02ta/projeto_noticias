321c10127f0ee3f1689a34ce77354ab14fa16390
nathallyleao02ta <nathallybarbosasaoleao@gmail.com>
2025-06-01 22:43:04 -0300

    javascript para abrir e fechar o menu completo

---
+++ script.js
@@ -0,0 +1,12 @@
+const botaoAbrir = document.querySelector(".header-mobile > button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector(".header-mobile nav")
+
+console.log(body)
+botaoAbrir.addEventListener('click', abrirMenu)
+
+function abrirMenu() {
+  body.
+
+}
